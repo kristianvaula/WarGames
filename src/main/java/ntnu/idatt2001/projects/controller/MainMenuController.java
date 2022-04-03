b5c2070a6263ff2465ecd4a168b5ff709b02a722
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-03 15:05:42 +0200

    Added MainMenuController.java

---
+++ src/main/java/ntnu/idatt2001/projects/controller/MainMenuController.java
@@ -0,0 +1,24 @@
+package ntnu.idatt2001.projects.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void exitToMainMenu(ActionEvent event) throws IOException {
+
+    }
+}
