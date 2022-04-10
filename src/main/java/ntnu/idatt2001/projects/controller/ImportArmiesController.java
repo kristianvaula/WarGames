200c89895a16640e529e1f4e4337863706692320
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-10 23:09:53 +0200

    Implemented default EditArmies.fxml, ImportArmies.fxml, SimulateBattle.fxml with controllers

---
+++ src/main/java/ntnu/idatt2001/projects/controller/ImportArmiesController.java
@@ -0,0 +1,44 @@
+package ntnu.idatt2001.projects.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ImportArmiesController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    /**
+     * Loads main menu when the exit button is pressed
+     * @param event button click
+     * @throws IOException If fxml load fails
+     */
+    @FXML
+    public void loadMainMenu(ActionEvent event) throws IOException {
+        try {
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/MainMenu.fxml"));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+
+            window.setScene(scene);
+            window.setResizable(false);
+            window.show();
+
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+            throw e;
+        }
+    }
+}
