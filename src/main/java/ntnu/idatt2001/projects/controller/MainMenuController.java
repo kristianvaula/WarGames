c76798a37c7fa2e25e00003d972be1670a88751a
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-13 10:56:42 +0200

    Minor changes

--- src/main/java/ntnu/idatt2001/projects/controller/MainMenuController.java
+++ src/main/java/ntnu/idatt2001/projects/controller/MainMenuController.java
@@ -55,8 +55,8 @@ public class MainMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try{
             ArmyFileHandler fileHandler = new ArmyFileHandler();
-            Army humanArmy = fileHandler.readArmyFromFile("Human Army");
-            Army orcArmy = fileHandler.readArmyFromFile("Orc Army");
+            Army humanArmy = fileHandler.getArmyFromFile("Human Army");
+            Army orcArmy = fileHandler.getArmyFromFile("Orc Army");
             battle = new Battle(humanArmy,orcArmy);
             displayArmy(battle.getArmyOne(),armyNameOutputA,armyUnitsOutputA);
             displayArmy(battle.getArmyTwo(),armyNameOutputB,armyUnitsOutputB);
