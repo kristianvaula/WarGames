9a8f9f1035e24add35bde7df8cd833d592f3f242
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-14 12:15:42 +0200

    Enum UnitType added toString method

--- src/main/java/ntnu/idatt2001/projects/model/units/UnitType.java
+++ src/main/java/ntnu/idatt2001/projects/model/units/UnitType.java
@@ -4,5 +4,22 @@ public enum UnitType {
     INFANTRY,
     CAVALRY,
     RANGED,
-    COMMANDER
+    COMMANDER;
+
+    @Override
+    public String toString() {
+        if(this == INFANTRY){
+            return "Infantry";
+        }
+        else if(this == CAVALRY){
+            return "Cavalry";
+        }
+        else if(this == RANGED){
+            return "Ranged";
+        }
+        else if(this == COMMANDER){
+            return "Commander";
+        }
+        return null;
+    }
 }
