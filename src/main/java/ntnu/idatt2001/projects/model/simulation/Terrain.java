cae38a2e0242f5075ae1fcdf727a5226087ecf2c
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-19 11:17:23 +0200

    Added Enum Terrain

---
+++ src/main/java/ntnu/idatt2001/projects/model/simulation/Terrain.java
@@ -0,0 +1,11 @@
+package ntnu.idatt2001.projects.model.simulation;
+
+/**
+ * Represents a terrain on which a unit stands on.
+ * Can affect their behaviour and effectiveness.
+ */
+public enum Terrain {
+    HILL,
+    PLAINS,
+    FOREST
+}
