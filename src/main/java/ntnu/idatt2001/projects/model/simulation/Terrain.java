7e7ad4c87ca250dc247ceccdcbe3574396f823ee
Kristian Vaula <kristvje@stud.ntnu.no>
2022-05-18 18:03:38 +0200

    Added getTerrains method that returns values as arrayList

--- src/main/java/ntnu/idatt2001/projects/model/simulation/Terrain.java
+++ src/main/java/ntnu/idatt2001/projects/model/simulation/Terrain.java
@@ -1,5 +1,9 @@
 package ntnu.idatt2001.projects.model.simulation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Represents a terrain on which a unit stands on.
  * Can affect their behaviour and effectiveness.
@@ -7,5 +11,14 @@ package ntnu.idatt2001.projects.model.simulation;
 public enum Terrain {
     HILL,
     PLAINS,
-    FOREST
+    FOREST;
+
+    /**
+     * Gets a list of all the terrains
+     *
+     * @return List of all Terrain values
+     */
+    public static List<Terrain> getTerrains(){
+        return new ArrayList<>(Arrays.asList(Terrain.values()));
+    }
 }
