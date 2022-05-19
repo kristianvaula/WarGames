a495a12509aed82331311116e09c34acfea2ddf9
Kristian Vaula <kristvje@stud.ntnu.no>
2022-05-19 21:50:10 +0200

    UnitType.java added getUnitTypes method

--- src/main/java/ntnu/idatt2001/projects/model/units/UnitType.java
+++ src/main/java/ntnu/idatt2001/projects/model/units/UnitType.java
@@ -1,10 +1,23 @@
 package ntnu.idatt2001.projects.model.units;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum UnitType {
     INFANTRY,
     CAVALRY,
     RANGED,
     COMMANDER;
+
+    /**
+     * Gets a list of all the unit types
+     *
+     * @return List of all UnitType values
+     */
+    public static List<UnitType> getUnitTypes(){
+        return new ArrayList<>(Arrays.asList(UnitType.values()));
+    }
 
     @Override
     public String toString() {
